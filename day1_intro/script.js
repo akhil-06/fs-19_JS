bd9ee133ce892b8aa9f01543d0f1a7440dd89373
akhil-06 <svam1008@gmail.com>
2024-05-06 21:48:42 +0530

    day 2 done

---
+++ day1_intro/script.js
@@ -0,0 +1,36 @@
+// console.log("hello world!!!");
+//implementing new feature :- stories
+// multiline comments:- /*  this is a multiline comment */ 
+// single line comments:- // this
+// console.log("hello world!!!");
+// console.log("hello world!!!");
+// console.log("hello world!!!");
+// console.log("hello world!!!");
+
+
+
+//variables:- global scope and block scope
+var name1 = "niharika";
+let name = "arhan";
+const name2 = "yatik";
+
+//data types
+// 7 primitive data types
+// number, boolean, null, undefined, strings, bigint, symbol
+let a = 10;
+let b = true;
+let c = "akhil";
+let d = null;
+let e = undefined;
+let f = BigInt(723549782534729354);
+let g = Symbol("akhi");
+// console.log(typeof a,typeof b,typeof c,typeof d,typeof e,typeof f,typeof g);
+
+var h = "Akhil sharma";
+h = 60;
+h = true;
+console.log(typeof h);
+// 1 non primitive data types
+// objects
+
+
---
+++ day2_operators/script.js
@@ -0,0 +1,97 @@
+// var a = null;
+// console.log(a);
+// a = "Akhil Sharma"
+// console.log(a);
+
+// 2015:- let/const, var
+// boolean a = 80;
+// let a = true;
+// let b = "Akhil Sharma";
+
+// let b = {}
+// // CRUD operations
+// let student = {
+//     name: 'Akhiill',
+//     age: 23,
+//     city: 'Delhi'
+// }
+// student.name = "Akhil";
+// student.number= 723;
+// console.log(student.number);
+// delete student;
+// console.log(student.name);
+// console.log(student.age);
+// console.log(student.city);
+// console.log(student.name);
+
+
+// conver celcius to farenhite in javascript
+// let t=25; 
+// let f=t*(9/5) +32; 
+// console.log(f);
+
+
+
+
+//Operators
+//Arithamatic
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2/3);
+// console.log(2%3);
+// console.log(2**3);
+
+// increment decrement operators
+let num = 5;
+//post:- first update in memory and then on console
+//pre:- first show on console and then update in memory
+// console.log(num--);
+// console.log(--num);
+// console.log(++num);
+// console.log(num++);
+
+
+//Assignment operators
+// =,+=,-=,*=,/=,%=,**=
+
+
+//Comparision operators
+// ==, !=, >, < , <=, >=, ===
+
+// == vs ===
+// console.log(2 === 2);
+
+
+// logical operators
+// && (AND), || (OR), ! (NOT)
+// console.log(true && false);
+// let a = 3;
+// let b = 4;
+// console.log(true && false);
+// console.log(a,b);
+
+
+
+
+//type conversion
+//implicit type conversion (type coercion)
+//Javascript automatically converts one data type to another if we try to assign or compare them without specifying
+// console.log(2 == "2");
+// console.log(typeof (35 + "hello"));
+
+//explicit type conversion (type casting)
+// let number = 12;
+// let stringConvert = Symbol(number);
+// console.log(typeof stringConvert);
+
+
+//swap two variables without using temporary variable
+// var b=5;
+// var a=2;
+// a=a+b;
+// b = a - b; 
+// a = a - b; 
+// console.log(a,b);
+
+
