4dc115a78aec8a86a21e442d5b991f93fbdfb204
akhil-06 <svam1008@gmail.com>
2024-05-28 21:57:09 +0530

    promises and callback hell

---
+++ day16_promises/script.js
@@ -0,0 +1,94 @@
+
+//promises
+fetchRandomJoke()
+.then((joke)=>{
+    console.log(joke);
+    return translateJoke(joke);
+})
+.then((translatedJoke)=>{
+    console.log(translatedJoke);
+    return postJoke(translatedJoke);
+})
+.then(()=>{
+    console.log("Joke is posted successfully");
+})
+.catch((error)=>{
+    console.log('something went wrong',error);
+})
+
+
+//callback hell
+fetchRandomJoke((joke) =>{
+    console.log(joke);
+
+    translateJoke(joke, (translatedJoke)=>{
+        console.log(translatedJoke);
+
+        postJoke(translatedJoke, ()=>{
+            console.log("Joke posted successfully!!!");
+        })
+    })
+})
+
+
+
+
+// let userLoggedIn = true;
+
+// function userLoggeddIn(){
+//     let promise = new Promise(function(res, rej){
+//         if(userLoggedIn === true){
+//             setTimeout(function(){
+//                 res('user is loggedIN');
+//                 }, 1000);
+//         }else{
+//             rej('user is not loggedIN');
+//         }
+//     });
+//     return promise;
+// }
+
+
+// userLoggeddIn()
+// .then(function(data){
+//     console.log(data);
+// })
+// .catch(function(data){
+//     console.log(data);
+// })
+// console.log(promise);
+
+
+
+
+
+
+
+
+
+
+// // console.log("hello world!!");
+
+// setTimeout(function(){
+//     console.log("SetTime1 is finished!!!");
+// }, 5000)
+
+// setTimeout(function(){
+//     console.log("SetTime2 is finished!!!");
+// }, 3000)
+
+// setTimeout(function(){
+//     console.log("SetTime3 is finished!!!");
+// }, 6000)
+
+// fetchRandomJoke((joke) =>{
+//     console.log(joke);
+
+//     translateJoke(joke, (translatedJoke)=>{
+//         console.log(translatedJoke);
+
+//         postJoke(translatedJoke, ()=>{
+//             console.log("Joke posted successfully!!!");
+//         })
+//     })
+// })
